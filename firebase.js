98d903798abe36b56c14073a15085f6b22780fee
Seif Boukherouf <63857203+SB17Codes@users.noreply.github.com>
2024-05-17 01:06:37 +0200

    Secrets are meant to be guarded

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getFirestore, getDoc, doc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCL4RUH2espy2GoPEHjacR0tyjhuRtHGP0",
-  authDomain: "v2bdb-423513.firebaseapp.com",
-  projectId: "v2bdb-423513",
-  storageBucket: "v2bdb-423513.appspot.com",
-  messagingSenderId: "193228962092",
-  appId: "1:193228962092:web:e4d1b0b6d0cb01f5465489",
-  measurementId: "G-TZ77B3H85X"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
